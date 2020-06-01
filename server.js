747b7b00fce4f64aa25eb37b386306b7b2dac174
jason-nordheim <jason.nordheim@gmail.com>
2020-06-01 11:10:37 -0600

    setup passport to read in error messages from passport-config and to use express-flash to display error messages to the user

--- server.js
+++ server.js
@@ -1,25 +1,71 @@
-const express = require('express')
-const app = express()
-const PORT = 3000; 
+/*
+    Load in our process variables if we are in
+    NOT in the production environment
+*/ 
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 
-app.set('view-engine', 'ejs')
+const express = require('express')
+const bcrypt = require('bcrypt')
+const flash = require('express-flash')
+const session = require('express-session')
+const passport = require('passport')
+const app = express()
+const PORT = 3000 
 
 const users = []
 
-app.get('/', (request, response) => {
-    response.render('index.ejs', {name: "kyle"})
+
+const initializePassport = require('./passport-config')
+initializePassport(passport, email => users.find(user => user.email === email))
+
+app.set('view-engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: false })) // allows access to form data as part of the request 
+
+// setup passport 
+app.use(flash()) 
+app.use(session({
+    secret: process.env.SESSION_SECRET, 
+    resave: false,  // re-save values, even if they have not changed 
+    saveUninitialized: false // save unused values 
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {name: "kyle"})
 })
 
-app.get('/login', (request, response) => {
-    response.render('login.ejs')
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/', // what to do when authentication is successful 
+    failureRedirect: '/login',  // what to do when authentication fails 
+    failureFlash: true  // should display error messages provided in the done function
+})) 
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs')  
 })
 
-app.get('/register', (request, response) => {
-  response.render('register.ejs')  
-})
-
-app.post('/register',(request, response) => {
-
+app.post('/register', async (req, res) => {
+    try {
+        //const user = { name: req.body.gitname, password: req.body.password }
+        const hashedPassword = await bcrypt.hash(req.body.password, 12) 
+        users.push({
+            id: Date.now().toString(), 
+            name: req.body.name, 
+            email: req.body.email, 
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch (error) {
+        res.redirect('/register')
+    }
+    console.log(users)
 })
 
 app.listen(PORT, console.log(`Listening on ${PORT}`))
